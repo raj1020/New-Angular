99a5140b9e1b498f6bf54e7a35f15afec6226f96
raj1020 <raj.1020@hotmail.com>
2020-02-28 12:42:57 +1100

    added property binding features

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -9,14 +9,21 @@ import { Component, OnInit } from '@angular/core';
     <h2>The length of the name is {{name.length}}</h2>
     <h2>Your name in UpperCase {{name.toUpperCase()}}</h2>
     <h2>{{greetUser()}}</h2>
+    <h2>The url of this webpage is "{{siteUrl}}"</h2>
+    <input [id]= "myId" type= "text" value= "Paul">
+    <div><p></p></div>
+    <input [disabled] = "isDisabled" id= "{{myId}} type= "text value= "Paul">
 
   </div>`,
   styleUrls: ['./test.component.scss']
 })
 export class TestComponent implements OnInit {
 
-  public name = "Michael"
-
+  public name = "Michael";
+  public siteUrl = window.location.href;
+  public myId = "testId";
+  public isDisabled = true;
+  
   constructor() { }
 
   ngOnInit(): void {
